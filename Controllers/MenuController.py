b6e3ce7b95fca7534bc21fdaf372c2f30925982c
Noussai24 <noussaiba.ayadi@gmail.com>
2024-04-30 12:23:25 +0200

    👌 IMPROVE: le fichier est clean selon flak8

--- Controllers/MenuController.py
+++ Controllers/MenuController.py
@@ -45,8 +45,8 @@ class MenuController:
                 self.user_choice()
 
     def report_menu_choice(self):
-        """
-        Gère le choix de l'utilisateur dans le sous-menu des rapports et appelle les méthodes correspondantes.
+        """Gère le choix de l'utilisateur dans le sous-menu des rapports
+        et appelle les méthodes correspondantes.
         """
         choice = self.menu.report_menu()
 
